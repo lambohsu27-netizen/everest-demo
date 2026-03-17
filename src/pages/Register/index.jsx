13ffb3c367c8f9626d60f81f27d1a6a5bf2dd487
Elys Maldov <maldovelys@gmail.com>
2026-03-17 11:14:02 +0700

    feat(register): reset activeStep to 1

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -8,7 +8,7 @@ import RegisterForm from './RegisterForm'
 
 function Register() {
   const { currentModal, handleCurrentModal } = useRegister()
-  const [activeStep, setActiveStep] = useState(4)
+  const [activeStep, setActiveStep] = useState(1)
 
   return (
     <>

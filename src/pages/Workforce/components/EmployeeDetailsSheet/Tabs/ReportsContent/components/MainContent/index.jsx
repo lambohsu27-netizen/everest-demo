593a714cd086ee37d5667eef2db37a5cdadccd74
Elys Maldov <maldovelys@gmail.com>
2026-03-26 16:56:37 +0700

    feat(Workforce/MainContent): remove dividers

--- src/pages/Workforce/components/EmployeeDetailsSheet/Tabs/ReportsContent/components/MainContent/index.jsx
+++ src/pages/Workforce/components/EmployeeDetailsSheet/Tabs/ReportsContent/components/MainContent/index.jsx
@@ -8,11 +8,8 @@ export default function MainContent() {
   return (
     <div className="flex flex-col gap-10 w-full">
       <CreditSummarySection />
-      <hr className="border-gray-200" />
       <CreditOverviewSection />
-      <hr className="border-gray-200" />
       <LoanCategorySection />
-      <hr className="border-gray-200" />
       <NegativeEventsSection />
     </div>
   )
